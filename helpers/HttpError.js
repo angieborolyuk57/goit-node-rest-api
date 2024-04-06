5805c9ef8c29752df66803527acfa485072e6d57
Anhelina Rabtsevych <anhelina@ondaytrip.com>
2024-04-06 11:32:53 +0200

    merge errors fixed :)

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -4,24 +4,12 @@ const messageList = {
   403: "Forbidden",
   404: "Not Found",
   409: "Conflict",
-<<<<<<< HEAD
 }
 
-const HttpError = (status, message = errorMessageList[status]) => {
+const HttpError = (status, message = messageList[status]) => {
   const error = new Error(message)
   error.status = status
   return error
 }
 
-=======
-};
-
-class HttpError extends Error {
-  constructor(status, message = messageList[status]) {
-    super(message);
-    this.status = status;
-  }
-}
-
->>>>>>> 3ac0428 (added controllers, schema and model for auth, error handling)
-module.exports = HttpError;
+module.exports = HttpError
